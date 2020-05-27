26461586bfdbb020867711013adef8b1ed18214a
wUNDEER <wunder1337@gmail.com>
2020-05-27 19:24:16 +0200

    Updates

---
+++ precache-manifest.fe46e8c4f806a1cb7964f12ecc292aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4792369c862f73a7846eb9c8a058984",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "4a090b1ed841a94ea8df",
+    "url": "/react/static/css/main.5abd3554.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/react/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a090b1ed841a94ea8df",
+    "url": "/react/static/js/main.e8f7bad5.chunk.js"
+  },
+  {
+    "revision": "e361f9f3976e228ea696",
+    "url": "/react/static/js/runtime-main.2844bc1f.js"
+  }
+]);
